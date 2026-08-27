92d6a79df20ca67d9e82a66858ed333879319e0f
kadek <ikadekrioap@gmail.com>
2026-08-27 21:20:17 +0700

    test mobile also add some background di base web

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -43,7 +43,7 @@ useEffect(() => {
 
 
   return (
-    <section className="relative w-full h-screen mx-auto">
+    <section className="relative w-full h-screen mx-auto bg-[#050816]">
       <div className={` ${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className="flex flex-col items-center justify-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
@@ -68,25 +68,25 @@ useEffect(() => {
         </div>
       </div>
 
-      <ComputersCanvas />
+      {/* <ComputersCanvas /> */}
 
-<div className="absolute left-0 z-10 flex justify-center w-full bottom-2">
-  <a href="#about">
-    <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
-      <motion.div
-        animate={{
-          y: [0, 24, 0],
-        }}
-        transition={{
-          duration: 1.5,
-          repeat: Infinity,
-          repeatType: "loop",
-        }}
-        className="w-3 h-3 rounded-full bg-secondary"
-      />
-    </div>
-  </a>
-</div>
+      <div className="absolute left-0 z-10 flex justify-center w-full bottom-2">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   )
 }
