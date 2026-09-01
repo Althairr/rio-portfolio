71508f4c4d738fbc2adfe4d9064905f27211aa23
kadek <ikadekrioap@gmail.com>
2026-09-01 20:10:33 +0700

    test mobile webgl #3

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,82 +4,82 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-// const Computers = ({ isMobile }) => {
-//   const computer = useGLTF("/desktop_pc/scene.gltf");
+const Computers = ({ isMobile }) => {
+  const computer = useGLTF("/desktop_pc/scene.gltf");
 
-//   return (
-//     <mesh>
-//       <hemisphereLight intensity={0.25} groundColor='black' />
-//       <spotLight
-//         position={[-20, 50, 10]}
-//         angle={0.12}
-//         penumbra={1}
-//         intensity={1}
-//         castShadow
-//         shadow-mapSize={1024}
-//       />
-//       <pointLight intensity={1} />
-//       <primitive
-//         object={computer.scene}
-//         scale={isMobile ? 0.6 : 0.65}
-//         position={isMobile ? [0, -2.8, -2.2] : [0, -2.8, -1.5]}
-//         rotation={[-0.01, -0.2, -0.1]}
-//       />
-//     </mesh>
-//   );
-// };
-
-// const ComputersCanvas = () => {
-//   const [isMobile, setIsMobile] = useState(false);
-
-//   useEffect(() => {
-//     const mediaQuery = window.matchMedia("(max-width: 500px)");
-
-//     setIsMobile(mediaQuery.matches);
-
-//     const handleMediaQueryChange = (event) => {
-//       setIsMobile(event.matches);
-//     };
-
-//     mediaQuery.addEventListener("change", handleMediaQueryChange);
-
-//     return () => {
-//       mediaQuery.removeEventListener("change", handleMediaQueryChange);
-//     };
-//   }, []);
-
-//   return (
-//     <Canvas
-//       frameloop="demand"
-//       shadows
-//       dpr={isMobile ? 1 : [1, 2]}
-//       camera={{ position: [20, 3, 5], fov: 25 }}
-//       gl={{
-//         preserveDrawingBuffer: true,
-//         powerPreference: "default",
-//       }}
-//     >
-//       <Suspense fallback={<CanvasLoader />}>
-//         <OrbitControls
-//           enableZoom={false}
-//           maxPolarAngle={Math.PI / 2}
-//           minPolarAngle={Math.PI / 2}
-//         />
-
-//         <Computers isMobile={isMobile} />
-//       </Suspense>
-
-//       <Preload all />
-//     </Canvas>
-//   );
-// };
-
-const ComputersCanvas = () => {
   return (
-    <div className="absolute inset-0 flex items-center justify-center">
-      <div className="w-40 h-40 bg-[#915EFF] rounded-2xl" />
-    </div>
+    <mesh>
+      <hemisphereLight intensity={0.25} groundColor='black' />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <pointLight intensity={1} />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.6 : 0.65}
+        position={isMobile ? [0, -2.8, -2.2] : [0, -2.8, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
   );
 };
+
+const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      dpr={isMobile ? 1 : [1, 2]}
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{
+        preserveDrawingBuffer: true,
+        powerPreference: "default",
+      }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+
+        <Computers isMobile={isMobile} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+// const ComputersCanvas = () => {
+//   return (
+//     <div className="absolute inset-0 flex items-center justify-center">
+//       <div className="w-40 h-40 bg-[#915EFF] rounded-2xl" />
+//     </div>
+//   );
+// };
 
 export default ComputersCanvas;
