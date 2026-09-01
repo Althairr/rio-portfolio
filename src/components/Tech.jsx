f065999c9d340049bc65211608c3afa2aebe183f
kadek <ikadekrioap@gmail.com>
2026-09-01 21:24:39 +0700

    test mobile webgl #10 nyesuain tech.jsx cukup render 6 bola aja. New idea

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
@@ -10,25 +11,41 @@ const Tech = () => {
   useEffect(() => {
     const mediaQuery = window.matchMedia("(max-width: 500px)");
 
-    const updateMobile = (event) => {
+    const handleChange = (event) => {
       setIsMobile(event.matches);
     };
 
     setIsMobile(mediaQuery.matches);
 
-    mediaQuery.addEventListener("change", updateMobile);
+    mediaQuery.addEventListener("change", handleChange);
 
     return () => {
-      mediaQuery.removeEventListener("change", updateMobile);
+      mediaQuery.removeEventListener("change", handleChange);
     };
   }, []);
 
   useEffect(() => {
-    if (isMobile) {
-      // Shuffle technologies dan ambil 6 secara random
-      const shuffled = [...technologies].sort(() => Math.random() - 0.5);
-      setMobileTechnologies(shuffled.slice(0, 6));
-    }
+    if (!isMobile) return;
+
+    const getRandomTechnologies = () => {
+      const shuffled = [...technologies].sort(
+        () => Math.random() - 0.5
+      );
+
+      return shuffled.slice(0, 6);
+    };
+
+    // Pilihan pertama
+    setMobileTechnologies(getRandomTechnologies());
+
+    // Ganti icon setiap 3 detik
+    const interval = setInterval(() => {
+      setMobileTechnologies(getRandomTechnologies());
+    }, 4000);
+
+    return () => {
+      clearInterval(interval);
+    };
   }, [isMobile]);
 
   const displayedTechnologies = isMobile
--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -10,21 +10,24 @@ import {
 
 import CanvasLoader from "../Loader";
 
-const Ball = (props) => {
-  const [decal] = useTexture([props.imgUrl]);
+const Ball = ({ imgUrl }) => {
+  const [decal] = useTexture([imgUrl]);
 
   return (
     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
       <ambientLight intensity={0.25} />
       <directionalLight position={[0, 0, 0.05]} />
+
       <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
+
         <meshStandardMaterial
-          color='#fff8eb'
+          color="#fff8eb"
           polygonOffset
           polygonOffsetFactor={-5}
           flatShading
         />
+
         <Decal
           position={[0, 0, 1]}
           rotation={[2 * Math.PI, 0, 6.25]}
@@ -40,8 +43,8 @@ const Ball = (props) => {
 const BallCanvas = ({ icon }) => {
   return (
     <Canvas
-      frameloop='demand'
-      dpr={[1, 2]}
+      frameloop="demand"
+      dpr={1}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
